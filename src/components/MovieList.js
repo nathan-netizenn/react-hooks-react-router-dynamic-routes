a27155cb5e7e6bba3e1164ad5410f6f2938669bb
nathan <nathanmageka6@gmail.com>
2023-04-27 10:51:09 +0300

    nathan

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,12 +1,10 @@
 import React from "react";
 import { Route } from "react-router-dom";
 import MoviesList from "./MoviesList";
-
 function MoviesPage({ movies }) {
   return (
     <div>
       <MoviesList movies={movies} />
-      <Route movies={movies}/>
     </div>
   );
 }
